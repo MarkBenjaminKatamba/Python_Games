cc534ce6b0e0516dc240a93d0f1d54d93e4f1548
K.A.T.A.M.B.A <45174550+MarkBenjaminKatamba@users.noreply.github.com>
2020-04-30 13:56:20 +0300

    Started on Rock, Paper, Scissors Game

---
+++ Rock_Paper_Scissors.py
@@ -0,0 +1,4 @@
+# Make a rock-paper-scissors game where it is the player vs the computer. 
+# The computer’s answer will be randomly generated, while the program will 
+# ask the user for their input. This project will better your understanding of 
+# while loops and if statements.
