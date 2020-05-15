f0b95535637e98709ea0dd9486098e949c504e4b
K.A.T.A.M.B.A <45174550+MarkBenjaminKatamba@users.noreply.github.com>
2020-05-15 12:45:05 +0300

    Added Game Description

---
+++ Sine_Vs_Cosine_Curve.py
@@ -0,0 +1,5 @@
+# For this project, you will have to generate a sine vs cosine curve. 
+# You will need to use the numpy library to access the sine and cosine functions. 
+# You will also need to use the matplotlib library to draw the curve. 
+# To make this more difficult, make the graph go from -360° to 360°, with there being a 180° 
+# difference between each point on the x-axis.
