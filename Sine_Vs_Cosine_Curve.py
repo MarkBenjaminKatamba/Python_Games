e277b1ae6746fdb9557c3503d33163088c955912
K.A.T.A.M.B.A <45174550+MarkBenjaminKatamba@users.noreply.github.com>
2020-05-27 00:02:40 +0300

    Started Learning about Numpy

--- Sine_Vs_Cosine_Curve.py
+++ Sine_Vs_Cosine_Curve.py
@@ -3,3 +3,4 @@
 # You will also need to use the matplotlib library to draw the curve. 
 # To make this more difficult, make the graph go from -360° to 360°, with there being a 180° 
 # difference between each point on the x-axis.
+
---
+++ numpyLibrary.py
@@ -0,0 +1,22 @@
+# Resources: 
+# 1. https://cs231n.github.io/python-numpy-tutorial/
+# 2. https://numpy.org/doc/stable/user/numpy-for-matlab-users.html
+# 3. https://numpy.org/install/
+# 4. https://python-poetry.org/docs/
+# Numpy
+# Numpy is the core library for scientific computing in Python. It provides a 
+# high-performance multidimensional array object, and tools for working with these arrays.
+
+# Arrays
+# A numpy array is a grid of values, all of the same type, and is indexed by a tuple of 
+# nonnegative integers. The number of dimensions is the rank of the array; the shape of 
+# an array is a tuple of integers giving the size of the array along each dimension.
+
+# We can initialize numpy arrays from nested Python lists, and access elements using square brackets:
+
+import numpy as np
+
+a = np.array([1, 2, 3])         # Create a rank 1 array
+print(type(a))                  # prints "<Class 'numpy.ndarray'>"
+print(a.shape)                  # prints ("3,")
+print(a[0], a[1], a[2])         # prints "1 2 3"
