2157eab0bed69cc1a438d33880a70dfcabbff40b
K.A.T.A.M.B.A <45174550+MarkBenjaminKatamba@users.noreply.github.com>
2020-06-02 21:48:41 +0300

    Game Completed!

--- Sine_Vs_Cosine_Curve.py
+++ Sine_Vs_Cosine_Curve.py
@@ -6,7 +6,29 @@
 
 # References: 1. https://pythontic.com/visualization/charts/sinewave
 #             2. https://www.mathopenref.com/triggraphcosine.html
+#             3. https://pythonforundergradengineers.com/plotting-sin-cos-with-matplotlib.html
 
 import numpy as np
 import matplotlib.pyplot as plot
 
+x = np.arange(-2*np.pi, 2*np.pi, 0.1) # Start, stop, step
+y = np.sin(x)
+y1 = np.cos(x)
+
+plot.plot(x,y,x,y1)
+
+plot.title('Sine Vs Cosine Curve')
+
+plot.xlabel('x Values from -2pi to 2pi', color = 'b')
+
+plot.ylabel('sin(X) and cos(x)', color = 'g')
+
+plot.legend(['sin(x)','cos(y)'])
+
+plot.grid(True, which='both')
+
+plot.axhline(y = 0, color = 'b')
+
+plot.axvline(x = 0, color = 'g')
+
+plot.show()
