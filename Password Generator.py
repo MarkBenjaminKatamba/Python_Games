2c5d0bbbb1deac8088da9ea82f3f0d11aff1065c
K.A.T.A.M.B.A <45174550+MarkBenjaminKatamba@users.noreply.github.com>
2020-06-04 22:14:46 +0300

    Create Password Generator.py

---
+++ Password Generator.py
@@ -0,0 +1,8 @@
+# Write a programme, which generates a random password for the user. 
+# Ask the user how long they want their password to be, and how many 
+# letters and numbers they want in their password. Have a mix of upper 
+# and lowercase letters, as well as numbers and symbols. The password 
+# should be a minimum of 6 characters long.
+
+import random as rm
+
