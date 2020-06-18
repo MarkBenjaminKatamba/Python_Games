1e09294429810899fd6e3f933ee7b2d76cef32d1
K.A.T.A.M.B.A <45174550+MarkBenjaminKatamba@users.noreply.github.com>
2020-06-18 21:29:11 +0300

    Update Password.py
    God some guiding code for Password_Generator.py

--- Password.py
+++ Password.py
@@ -11,25 +11,39 @@
 # # Random string generator: https://www.educative.io/edpresso/how-to-generate-a-random-string-in-python?aid=5082902844932096&utm_source=google&utm_medium=cpc&utm_campaign=edpresso-dynamic&gclid=EAIaIQobChMI9bjDysOr6QIVRe7tCh2V1wNxEAAYASAAEgJVnfD_BwE
 
 # ==========================================================================
+# import random
+# import string
+
+# # printing lowercase
+# letters = string.ascii_lowercase
+# print ( ''.join(random.choice(letters) for i in range(10)) )
+
+# # printing uppercase
+# letters = string.ascii_uppercase
+# print ( ''.join(random.choice(letters) for i in range(10)) )
+
+# # printing letters
+# letters = string.ascii_letters
+# print ( ''.join(random.choice(letters) for i in range(10)) )
+
+# # printing digits
+# letters = string.digits
+# print ( ''.join(random.choice(letters) for i in range(10)) )
+
+# # printing punctuation
+# letters = string.punctuation
+# print ( ''.join(random.choice(letters) for i in range(10)) )
+# ========================================================================
+
+
 import random
 import string
 
-# printing lowercase
-letters = string.ascii_lowercase
-print ( ''.join(random.choice(letters) for i in range(10)) )
+def randomStringwithDigitsAndSymbols(stringLength=10):
+    password_characters = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(password_characters) for i in range(stringLength))
 
-# printing uppercase
-letters = string.ascii_uppercase
-print ( ''.join(random.choice(letters) for i in range(10)) )
-
-# printing letters
-letters = string.ascii_letters
-print ( ''.join(random.choice(letters) for i in range(10)) )
-
-# printing digits
-letters = string.digits
-print ( ''.join(random.choice(letters) for i in range(10)) )
-
-# printing punctuation
-letters = string.punctuation
-print ( ''.join(random.choice(letters) for i in range(10)) )
+print("Generating Random String password with letters, digits and special characters ")
+print ("First Random String ", randomStringwithDigitsAndSymbols() )
+print ("Second Random String", randomStringwithDigitsAndSymbols(10) )
+print ("Third Random String", randomStringwithDigitsAndSymbols(10) )
