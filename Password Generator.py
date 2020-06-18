60d53d0e64c048a6d305246818b5d338218ae67c
K.A.T.A.M.B.A <45174550+MarkBenjaminKatamba@users.noreply.github.com>
2020-06-18 21:27:57 +0300

    Update Password Generator.py
    Game's almost done, yaay!!!

--- Password Generator.py
+++ Password Generator.py
@@ -4,5 +4,33 @@
 # and lowercase letters, as well as numbers and symbols. The password 
 # should be a minimum of 6 characters long.
 
-import random as rm
+import random
+import string
 
+pass_len = input("Enter the desired password length:\n")
+
+pass_num = input("How many digits in password:\n")
+                 
+pass_sym = input("How many symbols in password:\n")
+
+pass_letters = input("How many letters in password:\n") 
+
+def Password():
+    
+    pass_num1 = string.digits
+    ''.join(random.choice(pass_num1) for i in range(int(pass_num)))
+    
+    pass_sym1 = string.punctuation
+    ''.join(random.choice(pass_sym1) for i in range(int(pass_sym)))
+    
+    pass_letters1 = string.ascii_letters
+    ''.join(random.choice(pass_letters1) for i in range(int(pass_letters)))
+    
+    pass_char = pass_num1 + pass_sym1 + pass_letters1
+    
+    return ''.join(random.choice(pass_char) for i in range(int(pass_len)))
+
+print("Your new password is ", Password())
+
+# letters = string.punctuation
+# print ( ''.join(random.choice(letters) for i in range(10)) )
