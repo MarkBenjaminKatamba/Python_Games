84239e6387a27ffddcbb02257a4d1bad9dbbce21
K.A.T.A.M.B.A <45174550+MarkBenjaminKatamba@users.noreply.github.com>
2020-06-22 11:03:44 +0300

    Create Hangman.py

---
+++ Hangman.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 22 10:59:09 2020
+
+@author: kmark
+"""
+
+'''
+This is probably the hardest one out of these 6 small projects. 
+This will be similar to guessing the number, except we are guessing the word. 
+The user needs to guess letters, Give the user no more than 6 attempts for 
+guessing wrong letter. This will mean you will have to have a counter. 
+You can download a ‘sowpods’ dictionary file or csv file to use as a way to 
+get a random word to use.
+'''
