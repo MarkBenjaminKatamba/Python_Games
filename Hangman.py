191a25572c1653f30da8b40012eda08efa304f4c
K.A.T.A.M.B.A <45174550+MarkBenjaminKatamba@users.noreply.github.com>
2020-06-24 12:20:16 +0300

    Started working on the game

--- Hangman.py
+++ Hangman.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jun 22 10:59:09 2020
-
-@author: kmark
-"""
-
 '''
 This is probably the hardest one out of these 6 small projects. 
 This will be similar to guessing the number, except we are guessing the word. 
@@ -12,4 +5,15 @@ The user needs to guess letters, Give the user no more than 6 attempts for
 guessing wrong letter. This will mean you will have to have a counter. 
 You can download a ‘sowpods’ dictionary file or csv file to use as a way to 
 get a random word to use.
+===============================================================================
+Useful Resource Links:
+    1. https://thewordsearch.com/hangman
+    2. https://en.wikipedia.org/wiki/Hangman_(game)
+    3. a) https://www.downloadexcelfiles.com/wo_en/download-excel-file-list-african-countries-dependent-territories#.XvC3yGgzbDc 
+       b) en.wikipedia.org/wiki/List_of_African_countries_by_population							
 '''
+import random
+import string
+
+def secretWord():
+    
