fda7b872c0b370cbcf0535bbd0a8b947efa5b93f
K.A.T.A.M.B.A <45174550+MarkBenjaminKatamba@users.noreply.github.com>
2020-07-17 00:28:50 +0300

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -12,8 +12,32 @@ Useful Resource Links:
     3. a) https://www.downloadexcelfiles.com/wo_en/download-excel-file-list-african-countries-dependent-territories#.XvC3yGgzbDc 
        b) en.wikipedia.org/wiki/List_of_African_countries_by_population							
 '''
+
 import random
 import string
+import AfricanCountries.csv
 
-def secretWord():
+def guess_word():
+    secretWord = random.choice(AfricanCountries.csv) # randomly choosing a word from the imported list
+    return secretWord.upper()       # converting all user input to uppercase
+
+# For the actual interactive game play, we need the function below, within
+# which we'll create several variables that we'll be updating during each turn
+# the user takes.
+    
+def GamePlay(secretWord):
+    wordCompletion = ' _ ' * len(secretWord) # So the secret word will initially contain only underscores
+    guessed = False
+    guessedLetters = []     # This list will hold the letters the user guesses
+    guessedWord = []        # This will hold the words the user guesses
+    Trials = 6
+    print("Let's play Hangman!")
+    print(wordCompletion)   # Printing the initial state of the game with all underscores
+    print("\n")
+# So we need a while loop that will run until either the word is guessed or the user runs out of guesses.    
+    
+
+
+
+
     
