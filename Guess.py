a718c6547a93978c59ac3f5e2f6bfa262a19abfd
Mark Benjamin Katamba <katamba@awamo.com>
2022-08-29 09:42:46 +0300

    Update Guess.py

--- Guess.py
+++ Guess.py
@@ -1,20 +1,21 @@
-# Working with While loops (Resource: https://www.digitalocean.com/community/tutorials/how-to-construct-while-loops-in-python-3)
+# Working with While loops 
+# (Resource: https://www.digitalocean.com/community/tutorials/how-to-construct-while-loops-in-python-3)
 
 import random
 
-number = random.randint(1, 25)
+number = random.randint(1, 30)
 
 number_of_guesses = 0
 
-while number_of_guesses < 5:
-    print('Guess a number between 1 and 25:')
+while number_of_guesses < 10:
+    print('Guess a number between 1 and 30:')
     guess = input()
     guess = int(guess)
 
     number_of_guesses = number_of_guesses + 1
 
-    if guess < 1 or guess > 25:
-        print('Sorry, your guess is out of scope!')
+    if guess < 1 or guess > 30:
+        print("Your guess is out of range.")
 
     elif guess < number:
         print('Your guess is too low')
@@ -25,6 +26,8 @@ while number_of_guesses < 5:
     elif guess == number:
         break
 
+if guess == number:
+    print('CONGRATULATIONS! You guessed the number in ' + str(number_of_guesses) + ' tries!')
 
 if guess == number:
     print('Congratulations, You guessed the number in ' + str(number_of_guesses) + ' tries!!!')
