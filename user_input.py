e8a2217864d613f0af5c903d5eb95c28a02a6aa8
Mark Benjamin Katamba <katamba@awamo.com>
2022-08-30 09:58:30 +0300

    Update user_input.py

--- user_input.py
+++ user_input.py
@@ -8,10 +8,10 @@ choice = input("Enter 1 for addition.\nEnter 2 for subtraction.\nEnter 3 for Mul
 choice = int(choice)
  
 if choice == 1:
-    print(f'You entered {v1} and {v2} and their addition is {v1 + v2}')
+    print(f'You entered {v1} and {v2} and their addition is: \n {v1 + v2}')
 elif choice == 2:
-    print(f'You entered {v1} and {v2} and their subtraction is {v1 - v2}')
+    print(f'You entered {v1} and {v2} and their subtraction is: \n {v1 - v2}')
 elif choice == 3:
-    print(f'You entered {v1} and {v2} and their multiplication is {v1 * v2}')
+    print(f'You entered {v1} and {v2} and their product is: \n {v1 * v2}')
 else:
-    print("Wrong Choice, terminating the program.")
+    print("Wrong Choice, terminating the program...")
