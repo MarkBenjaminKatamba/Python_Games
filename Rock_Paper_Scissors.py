c6c913e54e11176ede4a7ab8bfd089dfbe35d44c
Mark Benjamin Katamba <katamba@awamo.com>
2022-08-30 10:09:12 +0300

    Update Rock_Paper_Scissors.py

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -3,13 +3,6 @@
 # ask the user for their input. 
 # This project will better your understanding of while loops and if statements.
 
-# How to use random.choice() function to randomly select things from a list
-# (Resource: https://pynative.com/python-random-choice/)
-# import random
-# movie_list = ['The Godfather', 'The Wizard of Oz', 'Citizen Kane', 'The Shawshank Redemption', 'Pulp Fiction']
-# moview_item = random.choice(movie_list)
-# print ("Randomly selected item from list is - ", moview_item)
-
 import random
 
 Comp_Play = ['rock', 'paper', 'scissors']
