310ea04f35453da2a979ed8581a55fda589820d4
Mark Benjamin Katamba <katamba@awamo.com>
2022-09-20 00:08:26 +0300

    Update Rock_Paper_Scissors_Refined.py

--- Rock_Paper_Scissors_Refined.py
+++ Rock_Paper_Scissors_Refined.py
@@ -11,35 +11,178 @@ while gameRounds < 5:
     myGuess = str(input())
     compGuess = random.choice(compPlay)
 
-    if myGuess == ('rock' or 'Rock' or 'ROCK') and compGuess == ('scissors' or 'Scissors' or 'SCISSORS'):
+    if myGuess == 'rock' and compGuess == 'scissors':
         print(compGuess)
         myScore += 5
         print(f'Rock crushes scissors, you WIN {myScore} points for that! \n')
 
-    elif myGuess == ('scissors' or 'Scissors' or 'SCISSORS') and compGuess == ('rock' or 'Rock' or 'ROCK'):
+    elif myGuess == 'Rock' and compGuess == 'Scissors':
+        print(compGuess)
+        myScore += 5
+        print(f'Rock crushes scissors, you WIN {myScore} points for that! \n')
+
+    elif myGuess == 'ROCK' and compGuess == 'SCISSORS':
+        print(compGuess)
+        myScore += 5
+        print(f'Rock crushes Scissors, you WIN {myScore} points for that! \n')
+
+    elif myGuess == 'rock' and compGuess == 'scissors':
+        print(compGuess)
+        myScore += 5
+        print(f'Rock crushes Scissors, you WIN {myScore} points for that! \n')
+
+    elif myGuess == 'ROCK' and compGuess == 'scissors':
+        print(compGuess)
+        myScore += 5
+        print(f'Rock crushes Scissors, you WIN {myScore} points for that! \n')
+
+    elif myGuess == 'rock' and compGuess == 'SCISSORS':
+        print(compGuess)
+        myScore += 5
+        print(f'Rock crushes Scissors, you WIN {myScore} points for that! \n')
+
+    elif myGuess == 'Rock' and compGuess == 'scissors':
+        print(compGuess)
+        myScore += 5
+        print(f'Rock crushes Scissors, you WIN {myScore} points for that! \n')
+
+    elif myGuess == 'rock' and compGuess == 'Scissors':
+        print(compGuess)
+        myScore += 5
+        print(f'Rock crushes Scissors, you WIN {myScore} points for that! \n')
+
+# =========================================================================================
+
+    elif myGuess == 'scissors' and compGuess == 'rock':
         print(compGuess)
         compScore += 5
         print('Rock crushes scissors, you lose! \n')
 
-    elif myGuess == ('paper' or 'Paper' or 'PAPER') and compGuess == ('rock' or 'Rock' or 'ROCK'):
+    elif myGuess == 'Scissors' and compGuess == 'Rock':
+        print(compGuess)
+        compScore += 5
+        print('Rock crushes scissors, you lose! \n')
+
+    elif myGuess == 'SCISSORS' and compGuess == 'ROCK':
+        print(compGuess)
+        compScore += 5
+        print('Rock crushes scissors, you lose! \n')
+
+    elif myGuess == 'scissors' and compGuess == 'ROCK':
+        print(compGuess)
+        compScore += 5
+        print('Rock crushes scissors, you lose! \n')
+
+    elif myGuess == 'SCISSORS' and compGuess == 'rock':
+        print(compGuess)
+        compScore += 5
+        print('Rock crushes scissors, you lose! \n')
+
+    elif myGuess == 'Scissors' and compGuess == 'ROCK':
+        print(compGuess)
+        compScore += 5
+        print('Rock crushes scissors, you lose! \n')
+
+    elif myGuess == 'SCISSORS' and compGuess == 'Rock':
+        print(compGuess)
+        compScore += 5
+        print('Rock crushes scissors, you lose! \n')
+
+    elif myGuess == 'Scissors' and compGuess == 'rock':
+        print(compGuess)
+        compScore += 5
+        print('Rock crushes scissors, you lose! \n')
+
+    # =========================================================================================
+
+    elif myGuess == 'paper' and compGuess == 'rock':
         print(compGuess)
         myScore += 5
         print(f'Paper covers rock, you WIN! {myScore} points for that! \n')
 
-    elif myGuess == ('rock' or 'Rock' or 'ROCK') and compGuess == ('paper' or 'Paper' or 'PAPER'):
+    elif myGuess == 'Paper' and compGuess == 'Rock':
         print(compGuess)
-        compScore += 5
-        print('Paper covers rock, you lose! \n')
+        myScore += 5
+        print(f'Paper covers rock, you WIN! {myScore} points for that! \n')
 
-    elif myGuess == ('scissors' or 'Scissors' or 'SCISSORS') and compGuess == ('paper' or 'Paper' or 'PAPER'):
+    elif myGuess == 'PAPER' and compGuess == 'ROCK':
+        print(compGuess)
+        myScore += 5
+        print(f'Paper covers rock, you WIN! {myScore} points for that! \n')
+
+    elif myGuess == 'Paper' and compGuess == 'ROCK':
+        print(compGuess)
+        myScore += 5
+        print(f'Paper covers rock, you WIN! {myScore} points for that! \n')
+
+    elif myGuess == 'paper' and compGuess == 'Rock':
+        print(compGuess)
+        myScore += 5
+        print(f'Paper covers rock, you WIN! {myScore} points for that! \n')
+
+    elif myGuess == 'paper' and compGuess == 'ROCK':
+        print(compGuess)
+        myScore += 5
+        print(f'Paper covers rock, you WIN! {myScore} points for that! \n')
+
+    elif myGuess == 'PAPER' and compGuess == 'Rock':
+        print(compGuess)
+        myScore += 5
+        print(f'Paper covers rock, you WIN! {myScore} points for that! \n')
+
+    elif myGuess == 'Paper' and compGuess == 'rock':
+        print(compGuess)
+        myScore += 5
+        print(f'Paper covers rock, you WIN! {myScore} points for that! \n')
+
+    elif myGuess == 'PAPER' and compGuess == 'rock':
+        print(compGuess)
+        myScore += 5
+        print(f'Paper covers rock, you WIN! {myScore} points for that! \n')
+
+# ==================================================================================================
+
+    elif myGuess == 'scissors' and compGuess == 'paper':
         print(compGuess)
         myScore += 5
         print(f'Scissor cuts paper, you WIN! {myScore} points for that! \n')
 
-    elif myGuess == ('paper' or 'Paper' or 'PAPER') and compGuess == ('scissors' or 'Scissors' or 'SCISSORS'):
+    elif myGuess == 'Scissors' and compGuess == 'Paper':
         print(compGuess)
-        compScore += 5
-        print('Scissor cuts paper, you lose! \n')
+        myScore += 5
+        print(f'Scissor cuts paper, you WIN! {myScore} points for that! \n')
+
+    elif myGuess == 'SCISSORS' and compGuess == 'PAPER':
+        print(compGuess)
+        myScore += 5
+        print(f'Scissor cuts paper, you WIN! {myScore} points for that! \n')
+
+    elif myGuess == 'SCISSORS' and compGuess == 'Paper':
+        print(compGuess)
+        myScore += 5
+        print(f'Scissor cuts paper, you WIN! {myScore} points for that! \n')
+
+    elif myGuess == 'Scissors' and compGuess == 'PAPER':
+        print(compGuess)
+        myScore += 5
+        print(f'Scissor cuts paper, you WIN! {myScore} points for that! \n')
+
+    elif myGuess == 'SCISSORS' and compGuess == 'paper':
+        print(compGuess)
+        myScore += 5
+        print(f'Scissor cuts paper, you WIN! {myScore} points for that! \n')
+
+    elif myGuess == 'scissors' and compGuess == 'PAPER':
+        print(compGuess)
+        myScore += 5
+        print(f'Scissor cuts paper, you WIN! {myScore} points for that! \n')
+
+    elif myGuess == 'scissors' and compGuess == 'Paper':
+        print(compGuess)
+        myScore += 5
+        print(f'Scissor cuts paper, you WIN! {myScore} points for that! \n')
+
+# =========================================================================================
 
     elif myGuess == compGuess:
         print(compGuess)
