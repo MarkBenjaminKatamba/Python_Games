f53abe670cbbd9e5fc7e42444a3427fa68252561
Mark Benjamin Katamba <katamba@awamo.com>
2023-01-21 22:16:31 +0300

    Update Guess.py

--- Guess.py
+++ Guess.py
@@ -3,34 +3,37 @@
 
 import random
 
-number = random.randint(1, 30)
+number = random.randint(1, 25)
 
 number_of_guesses = 0
 
-while number_of_guesses < 10:
-    print('Guess a number between 1 and 30:')
+while number_of_guesses < 5:
+    print('Guess a number between 1 and 25:')
     guess = input()
     guess = int(guess)
 
     number_of_guesses = number_of_guesses + 1
 
-    if guess < 1 or guess > 30:
-        print("Your guess is out of range.")
-
-    elif guess < number:
+    if number > guess > 1:
         print('Your guess is too low')
 
-    elif guess > number:
+    if number < guess < 25:
         print('Your guess is too high')
 
-    elif guess == number:
+    # if 25 < guess < 1:
+    #     print('Your guess is out of range!')
+
+    if guess < 1:
+        print('Your guess is out of range!')
+
+    if guess > 25:
+        print('Your guess is out of range!')
+
+    if guess == number:
         break
 
 if guess == number:
-    print('CONGRATULATIONS! You guessed the number in ' + str(number_of_guesses) + ' tries!')
+    print('Nice! You guessed the number on the ' + str(number_of_guesses) + 'th trial!')
 
-if guess == number:
-    print('Congratulations, You guessed the number in ' + str(number_of_guesses) + ' tries!!!')
 else:
     print('You did not guess the number. The number was ' + str(number))
-
